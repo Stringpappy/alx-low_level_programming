7dae180051a652652d0e36ddbadf2d516e557726
stringpappy <jejeodunayo70@gmail.com>
2023-12-17 15:17:12 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
