0734741a6e96ebf70845c0964acc4219daaa1ddf
stringpappy <jejeodunayo70@gmail.com>
2023-12-17 14:53:00 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int  x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+	printf("\n");
+
+	return (0);
+}
+
