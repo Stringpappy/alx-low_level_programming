2ea576d8753d590ba3cac23bde147d9f989ee45d
stringpappy <jejeodunayo70@gmail.com>
2023-12-17 13:05:38 -0800

    https://intranet.alxswe.com/projects/213#:~:text=julien%40ubuntu%3A~/0x01%24-,./2%2Dprint_alphabet,-abcdefghijklmnopqrstuvwxyz%0Ajulien%40ubuntu

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 'a'; x <= 'z'; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
