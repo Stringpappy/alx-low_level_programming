ce68b8cf340bd3485e49ac06ac7f2319d0f5e0ec
stringpappy <jejeodunayo70@gmail.com>
2023-12-17 14:15:39 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-/** 
- * main - Entry
- * Return: Always 0 (success)
+/**
+ * * main - Entry
+ * * Return: Always 0 (success)
  */
 int main(void)
 {
 	char lowcase, e, q;
-	 
+	
 	e = 'e';
 	q = 'q';
 
 	for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
 	{
-	if (lowcase != e && lowcase !=q)
+	if (lowcase != e && lowcase != q)
 	putchar(lowcase);
 	}
 	putchar('\n');
