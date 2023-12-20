7916b0712b2064a68500aabaad4e4dfcfd439f3e
stringpappy <jejeodunayo70@gmail.com>
2023-12-19 20:25:46 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * print_last_digit - printing last digit of a number
+ * @n: Number to be treated
+ * :return: The last digit value of a number
+ */
+
+int print_last_digit(int n)
+
+{
+
+	int l;
+
+	l = n % 10;
+
+	if (l < 0)
+	{
+	l = l * -1;
+	}
+	_putchar(l + '0');
+	return (l);
+}
