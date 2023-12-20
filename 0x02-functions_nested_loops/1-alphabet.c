f947b73a5552a5c69cd28f1735f696c522a1f647
stringpappy <jejeodunayo70@gmail.com>
2023-12-20 13:26:13 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - Print_alphabet  Instead of entry point
+* main - print_alphabet  Instead of entry point
 *Return: Always 0 (success)
 */
 void print_alphabet(void)
