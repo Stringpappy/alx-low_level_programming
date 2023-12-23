8daa796c0fffe7d106b12fa3588b9e54d1c8bc99
stringpappy <jejeodunayo70@gmail.com>
2023-12-22 16:11:22 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check fordigit through
+ * @a: variable a
+ * @b: variable b
+ * Return: return 0
+ */
+int mul(int a, int b)
+{
+
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
