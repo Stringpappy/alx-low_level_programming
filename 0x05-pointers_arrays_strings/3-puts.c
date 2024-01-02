818d67736b70a2828e7061e77534a2f7d8d139c2
stringpappy <jejeodunayo70@gmail.com>
2024-01-02 15:23:08 -0800

    i dont fear computer

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
-#includr "main.h"
+#include "main.h"
 
 /**
  * _puts - print a string
  * @str: string to print
  */
 
-void _puts(char *str);
+void _puts(char *str)
 {
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
-		_putchar
+		_putchar('\n');
+
 }
