155e9d2f9b59933dfa52f8a18c95ba3fbfcdd4ed
stringpappy <jejeodunayo70@gmail.com>
2024-01-03 09:24:11 -0800

    _strpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the string pointed by src
+ * @dest: copy  to
+ * @src: copy  fro
+ * Return : string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return(dest);
+}
