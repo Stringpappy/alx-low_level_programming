1f8179817708b8c0b5667694f821c1747d81cc92
stringpappy <jejeodunayo70@gmail.com>
2024-01-03 15:39:11 -0800

    puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,34 @@
-#include "main"
+#include "main.h"
 
 /**
  * puts_half - half of a string
- *
- * void puts_half(char *str);
+ * @str: paremeter
+ *Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int length;
+	int index;
+	int half_L;
+
+	length = 0;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		length += 1;
+	}
+	if (length % 2 == 0)
+	{
+		half_L = length / 2;
+	}
+	else
+	{
+		half_L = (length - 1) / 2;
+	}
+	for (index = half_L; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+		_putchar('\n');
+}
