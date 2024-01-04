dd318f4d4d907435b5c4fff6c9fc41e0f4e098f8
stringpappy <jejeodunayo70@gmail.com>
2024-01-04 07:18:56 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,10 +3,28 @@
 /**
  * *_strcat - function that concatenete two stringd
  * @dest: first string
- * @src: second string 
- * Return: ALways 0
+ * @src: second string
+ *  Return: ALways 0
  */
 
 char *_strcat(char *dest, char *src)
 {
-	char 
+	int a;
+	int j;
+
+	a = 0;
+	while ((dest[a] != '\0'))
+	{
+		a++;
+	}
+	j = 0;
+	while ((src[j] != '\0'))
+	{
+		dest[a] = src[j];
+		j++;
+		j++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unstd.h>
+
 #include <stdio.h>
+#include <unistd.h>
 
 
 int _putchar(char c);
