67ca6176b80042be6eb3c42aea705da7374f5351
stringpappy <jejeodunayo70@gmail.com>
2024-01-04 14:47:05 -0800

    change lower case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - funtion that shows the lowercase of a string
+ *@n: array
+ * Return: void
+ */
+char *string_toupper(char *n)
+{
+	int x;
+
+	x = 0;
+		while (n[x] != '\0')
+		{
+			if (n[x] >= 'a' && n[x] <= 'z')
+			{
+				n[x] = n[x] - 32;
+				x++;
+			}
+		}
+		return (n);
+}
