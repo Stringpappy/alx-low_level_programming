cee2ac05d9bc8c1f7eeec6e9946605c50fa28f4d
stringpappy <jejeodunayo70@gmail.com>
2024-01-04 05:57:05 -0800

    main. h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strcat - function that concatenete two stringd
+ * @dest: first string
+ * @src: second string 
+ * Return: ALways 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char 
