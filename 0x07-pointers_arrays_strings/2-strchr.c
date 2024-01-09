4161722bb8bd3aff78f2c3ea8c328053b58c0e5c
stringpappy <jejeodunayo70@gmail.com>
2024-01-09 10:40:52 -0800

    function that gets the length of a prefix substring.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "main.h"
+
+/**
+ * _strchr - FUNCTION THAT LOCATE A CHARRACTER IN A STRING
+ * @s:
+ * @c:
+ * Return:0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int start = 0;
+
+	for (; s[start] == c)
+	{
+		if (s[start] == c)
+
+			return (&s[start]);
+	}
+	return (0);
+}
