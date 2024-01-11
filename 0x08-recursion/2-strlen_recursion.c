52769e9279a7a2af12669818ac242dba491bab2f
stringpappy <jejeodunayo70@gmail.com>
2024-01-11 10:10:00 -0800

    lenght of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ * @s: pointer
+ * Return: always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+		return (_strlen_recursion(s) + 1);
+}
