8f64b7c77047a58ec036c5220e5ff9fe4b2af935
stringpappy <jejeodunayo70@gmail.com>
2024-01-16 09:43:50 -0800

    libmy

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *  _strspn -   function that gets the length of a prefix substring
+ *  @s:input
+ *  @accept: input
+ *  Return: Always n
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int root = 0;
+	int count;
+
+	while (*s)
+	{
+		for (count = 0; accept[count]; count++)
+		{
+			if (*s == accept[count])
+			{
+				root++;
+				break;
+			}
+			else if (accept[count + 1] == '\0')
+				return (root);
+		}
+		s++;
+	}
+	return (0);
+}
