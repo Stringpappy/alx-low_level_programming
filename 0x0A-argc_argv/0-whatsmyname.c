f4a57b6ec4ff5455e6f13917500df18762ba5b77
stringpappy <jejeodunayo70@gmail.com>
2024-01-16 15:42:06 -0800

    wht is myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - name of the programe
+ * @argc: argument count
+ * @argv:array of argument 
+ * Return: 0
+ */
+int main(int argc __attribute__((unsed)), *argv[])
+{
+	printf("%s\n", *argv);
+	return(0);
+}
