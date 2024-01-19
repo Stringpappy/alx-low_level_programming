f417aff349cf2e3f4b4afd1d8c6d0f72da6e2f97
stringpappy <jejeodunayo70@gmail.com>
2024-01-19 13:44:47 -0800

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,22 +19,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		y = 0;
 	else
-	{
 		for (y = 0; s2[y] != '\0'; y++)
 			;
-	}
 	if (y > n)
 	{
 		y = n;
 		s = malloc(sizeof(char) * (x + y + 1));
-		if (s == NULL)
-			return (NULL);
-		for (z = 0; z < x; z++)
-			s[z] = s1[z];
-		for (z = 0; z < y; z++)
-		{
-			s[z + x] = s2[z];
-			s[x + y] = '\0'; }
 	}
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+	{
+		s[z] = s1[z];
+	}
+	for (z = 0; z < y; z++)
+	{
+		s[z + x] = s2[z];
+		s[x + y] = '\0'; }
 	return (s);
 }
