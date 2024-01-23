16105b21ebba229f916a36d58812dd6e9fa72c99
stringpappy <jejodunayo70@gmail.com>
2024-01-23 12:59:52 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog informati0n
+ * @name: dog name
+ * @age:dog age
+ * @owner:dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
