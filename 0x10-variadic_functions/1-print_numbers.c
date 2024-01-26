bd7352448f67a7061230e0ec948231993b29cee0
stringpappy <jejodunayo70@gmail.com>
2024-01-26 02:40:21 -0800

    print num

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that printts numbers
+ * @separator: pointer
+ * @n: integers
+ *Retturn: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int count, i;
+	va_list folder;
+
+	va_start(folder, n);
+
+	for (count = 0; count < n; count++)
+	{
+		i = va_arg(folder, const unsigned int);
+		printf("%d", i);
+			if (i != (n - 1) && separator != NULL)
+			{
+				printf("%s", separator);
+			}
+	}
+		printf("\n");
+		va_end(folder);
+}
