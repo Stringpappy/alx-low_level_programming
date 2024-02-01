63950e7f681a8711f52fa9a1df367197c57f155c
stringpappy01 <jejeodunayo70@gmail.com>
2024-02-01 15:29:25 -0800

    0-printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t  nodes;
+	size_t  nodes = 0;
 
-	nodes = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
 		nodes++;
+		h = h->next;
 	}
 	return (nodes);
 }
