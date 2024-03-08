610c43b93ef815cff72ae03a31d3722d572c2583
stringpappy01 <jejeodunayo70@gmail.com>
2024-03-08 09:11:06 -0800

    dog details

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,5 @@ void print_dog(struct dog *d)
 		d->name = "(nill)";
 	if (d->owner == NULL)
 		d->owner = "(nill)";
-	if (d == NULL)
-		return;
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
