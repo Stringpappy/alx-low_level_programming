4db8cb27ae7d371db61d7ccb529159fe8670a6aa
stringpappy <jejeodunayo70@gmail.com>
2024-04-01 15:46:59 -0700

    n x number

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,21 +2,22 @@
 
 /**
  * print_times_table - Prints then  times, starting with 0
+ * @n: int to be inserted
  * Return: void
  */
 void print_times_table(int n)
 {
-	
-        int x, y, z;
+
+	int x, y, z;
 
 	if (n > 15 || n == 0)
-                return;
+		return;
 
 	for (x = 0; x <= n; x++)
-        {
-        for (y = 0; y <= n; y++)
-        {
-        z = x * y;
+	{
+	for (y = 0; y <= n; y++)
+	{
+	z = x * y;
 	if (z == 144)
 		printf("144");
 	else
