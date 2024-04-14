883a0206418c888cf856cb00ef3687a0456fa58f
stringpappy <jejeodunayo70@gmail.com>
2024-04-14 13:51:59 -0700

    more pointer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] ==  c)
-		{
-			return i; // return the index if character is found
-		}
-    }
-    return -1; // return -1 if character is not found
+        int start = 0;
+
+        for (; (s[start] == c);)
+        {
+                if (s[start] == c)
+
+                        return (&s[start]);
+        }
+        return (0);
 }
