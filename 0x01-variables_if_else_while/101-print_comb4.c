5351567a0896680d56dc65060ddb3abf440c6fe2
Stringpappy <jejeodunayo70@gmail.com>
2024-08-24 07:37:49 -0700

    if else statemeent

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,31 +4,29 @@
 
 /**
  * main - Entry point
- * Return: ALways 0 (success)
+ * Return: 0
  */
 int main(void)
 {
 	int x, y, z;
 
-	for (x = '0'; x < '9'; x++)
+	for (x = '0'; x <= '7'; x++)
 	{
-	for (y = x + 1; y <= '9'; y++)
-	{
-	for (z = y + 1; z <= '9'; z++)
-	{
-	if ((y != x) != z)
-	{
-	putchar(x);
-	putchar(y);
-	putchar(z);
-	if (x == '7' && y  == '8')
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
+		for (y = x + 1; y <= '8'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+			}
+			if (x != '7' || y != '8' || z != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
 }
-
-
